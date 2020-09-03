e5dea2abcb7f3b48a1a25f780336d32497adf275
tongzhenyu <tongzhenyu@hellobike.com>
2020-09-03 20:26:08 +0800

    更新

--- vue-cli-demo/src/views/Home.vue
+++ vue-cli-demo/src/views/Home.vue
@@ -13,6 +13,12 @@ export default {
   name: "Home",
   components: {
     HelloWorld
-  }
+  },
+  mounted() {
+    window.history.pushState({name: 'home-pushstate'}, null, window.location.href);
+    window.onpopstate = function(e) {
+      console.log(e)
+    }
+  },
 };
 </script>
